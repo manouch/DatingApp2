7bcd5e4008cdec528b2841feb4981e1d4f9c0d6b
manouch <manoujon@gmail.com>
2020-11-10 09:52:22 -0500

    End of section 7

--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -27,12 +27,7 @@ export class NavComponent implements OnInit {
       (response) => {
         // console.log(response);
         this.router.navigateByUrl('/members');
-      },
-      (error) => {
-        console.log(error);
-        this.toastr.error(error.error);
-      }
-    );
+      });
   }
 
   logout() {
